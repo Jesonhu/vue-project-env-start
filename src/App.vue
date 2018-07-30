4c80086cb3d172b612c740d99d1956dfbfa3d36c
Jeson <jesonhu_web@163.com>
2018-07-30 16:09:51 +0800

    feat: 路由验证，与状态管理使用

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,77 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img :src="localImg.logo" alt="">
+    <div class="img_wrap"></div>
     <router-view/>
+
+    <!-- 版权组件 start -->
+    <v-copyright class="text_center"></v-copyright>
+    <!-- 版权组件 end -->
   </div>
 </template>
 
 <script>
+import copyright from '@/components/copyright/copyright';
+
 export default {
   name: 'App',
+  data() {
+    return {
+      localImg: {
+        logo: './static/img/logo.png'
+      },
+      copyright: this.GLABAL_DATA.copyright
+    }
+  },
+  methods: {
+  },
+  components: {
+    vCopyright: copyright
+  }
 };
 </script>
 
 <style>
+/**
+ TIPS:
+ <style lang="scss" scoped> `scss`类型时需要指定作用域`scope`, 不指定不会生效;
+ <style> 不指定作用域也能生效
+ */
+
+/* init_mobile style set start */
+
+/* `盒模型` 解析方式设置 @see [Bootstrap](http://getbootstrap.com/docs/4.1/scss/_reboot.scss) */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* 最少占`一屏` */
+html,
+body {
+  width:100%;
+  height: 100%;
+}
+
+/* 文字居中 */
+.text_center {
+  text-align: center;
+}
+
+/* 清除浮动 */
+
+
+/* init_mobile style set end */
+
 #app {
+  position: relative;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/copyright/copyright.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="c_copyright">
+    <!-- {{copyright}} -->
+    {{GLABAL_DATA.copyright}}
+  </div>
+</template>
+
+<script>
+  export default {
+    /* 版权组件 */
+    name: 'copyright',
+    data() {
+      return {
+        // copyright: this.GLABAL_DATA.copyright
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .c_copyright {
+    position: absolute;
+    bottom: 20px;
+    left:0;
+    width:100%;
+    font-size: 12px;
+    width:100%;
+  }
+</style>
+
